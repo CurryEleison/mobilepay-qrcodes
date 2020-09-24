a269d17d422f85cedbc85b12b9516141b2b63ee5
Thomas Petersen <tpe@blackwoodseven.com>
2020-09-24 15:59:51 +0200

    Disabled i18next debugging in production

--- src/i18n.js
+++ src/i18n.js
@@ -4,12 +4,14 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const { REACT_APP_DEBUG_I18NEXT } = process.env;
+
 i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        debug: true,
+        debug: !!REACT_APP_DEBUG_I18NEXT,
         lng: 'en',
         fallbackLng: 'en',
         whitelist: ['en', 'da'],
